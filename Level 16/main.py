3df45365791b7a44c5c6ccd52071724ddeb7bb08
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-10-18 10:58:40 +0200

    Update main.py

--- Level 16/main.py
+++ Level 16/main.py
@@ -11,19 +11,23 @@ def connect_to_server(ip, port):
     s.connect((ip, port))
     return s
 
+
 def send_data_to_server(s, data):
     # s is a connected socket, data is a string
     data = str(data)
     data = data.encode("utf-8")
     s.send(data)
 
+
 def receive_data_from_server(s):
     # s is a connected socket, if there is no data to receive, this will hang for 5 seconds and then die
     data = s.recv(1024*10).decode("utf-8")
     return data
 
+
 def close_connection_to_server(s):
     s.close()
+
 
 def display_level():
     client_socket = connect_to_server("10.0.0.5", 1234)
@@ -51,8 +55,30 @@ def display_level():
     import json
 
     jsonObject = json.loads(challenge_input)
+    print(type(str(jsonObject)))
+
+    jsonAttribute = jsonObject[attribute]
+    print(jsonAttribute)
+
+    contains_string = False
+    if type(jsonAttribute) is str: 
+        for item in jsonAttribute:
+            if isinstance(item, str):
+                contains_string = True
+                print("contains string")
+                break
+
     
-    result = jsonObject[attribute]
+
+    result = jsonAttribute
+
+
+
+
+
+
+
+
 
 
     # send response to the server (response has to be stored in "result" by your code above)
